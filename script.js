6e97abdf684d3bb17394cf236ab58a27c6b1fb53
Davina Q. <quansahdavinaa@gmail.com>
2026-04-30 18:01:42 +0000

    git push

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-// let name1 = document.querySelector(n1).value;
-// let name2 = document.querySelector(n2).value;
-// let age = document.querySelector(age).value;
-// let grade = document.querySelector(grade).value;
-function changetext(){
+let name1 = document.querySelector(n1).value;
+let name2 = document.querySelector(n2).value;
+let age = document.querySelector(age).value;
+let grade = document.querySelector(grade).value;
+function portal(){
     const portal = document.getElementById(`p`).value;
     portal.innerHTML = `Admission successful. Welcome to Agape Academy!`;
 }
+//html interface, css styling aspect, js logic - functions
